5af8e3d5f8690a5177da90bdbcde83fa468ac18e
Glen Ford <glen.a.ford@gmail.com>
2010-09-25 21:40:20 +0100

    now builds with Lift-2.1 instead of SNAPSHOT also added sbt-plugin for idea to allow production of idea project files

--- project/build/vBaD.scala
+++ project/build/vBaD.scala
@@ -1,18 +1,12 @@
 import sbt._
 
-class vBaDProject(info: ProjectInfo) extends DefaultWebProject(info) {
-  val liftVersion = "2.1-SNAPSHOT"
+class vBaDProject(info: ProjectInfo) extends DefaultWebProject(info) with IdeaProject {
+  val liftVersion = "2.1"
   val commonsHttpVersion = "3.1"
   val jettyVersion = "6.1.22"
   val junitVersion = "4.5"
-  val specsVersion = "1.6.6-SNAPSHOT"
+  val specsVersion = "1.6.5"
   val h2databaseVersion = "1.2.138"
-
-  val scalatoolsSnapshot = "Scala Tools Snapshot" at "http://scala-tools.org/repo-snapshots/"
-
-  // If you're using JRebel for Lift development, uncomment
-  // this line
-  // override def scanDirectories = Nil
 
   override def libraryDependencies = Set(
     "net.liftweb" %% "lift-webkit" % liftVersion % "compile->default",
