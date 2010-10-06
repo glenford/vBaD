21a06a932b56cf38ef88cfffce8fb44af85ff2b1
glenford <glen.a.ford@gmail.com>
2010-10-06 15:28:56 +0100

    started working on proper specs

--- src/main/scala/net/usersource/vbad/lib/CruiseBuildStatus.scala
+++ src/main/scala/net/usersource/vbad/lib/CruiseBuildStatus.scala
@@ -2,12 +2,30 @@ package net.usersource.vbad.lib
 
 import xml.{XML, Elem, Node}
 import org.joda.time.format.{DateTimeFormat, DateTimeFormatter}
+import org.joda.time.DateTime
+
+
+case class CruiseJob( name: String, status: String, timestamp: DateTime )
+case class CruiseStage( name: String, jobs: List[CruiseJob] )
+case class CruisePipeline( name: String, stages: List[CruiseStage] )
+
+
+
+object CruisePipeline {
+
+  def apply( name: String, xmlAsString: String ): Option[CruisePipeline] = {
+    None
+  }
+
+}
+
+
 
 class CruiseBuildStatus( name:String, status: String, timestamp: String )
         extends BuildStatus(name,status,timestamp)
 
 object CruiseBuildFactory {
-  private val dtParser: DateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-ddTHH:mm:ss")
+  private val dtParser: DateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss")
 
   private def listFromXml(xmlAsString : String ): List[CruiseBuildStatus] = {
     listFromXml(XML.loadString(xmlAsString))
@@ -26,7 +44,10 @@ object CruiseBuildFactory {
   }
 
   def build( name:String, source: String ): Option[CruiseBuildStatus] = {
-    listFromXml(source).sort( (a,b) => dtParser.parseDateTime(a.timestamp).isBefore(dtParser.parseDateTime(b.timestamp))).headOption 
+    listFromXml(source).sort( (a,b) => dtParser.parseDateTime(a.timestamp).isBefore(dtParser.parseDateTime(b.timestamp))).headOption
   }
 
+  def buildPipeline(name:String, source: String ): List[CruiseBuildStatus] = {
+    listFromXml(source).filter( status => status.name.startsWith(name) )
+  }
 }
