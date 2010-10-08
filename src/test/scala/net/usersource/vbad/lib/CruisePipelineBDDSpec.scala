b6f5e81ce5f556fd2b6f7c326a91f5e9b9c53ce0
Glen Ford <glen.a.ford@gmail.com>
2010-10-08 22:45:44 +0100

    added some more test asserts

--- src/test/scala/net/usersource/vbad/lib/CruisePipelineBDDSpec.scala
+++ src/test/scala/net/usersource/vbad/lib/CruisePipelineBDDSpec.scala
@@ -21,8 +21,8 @@ class CruisePipelineBDDSpec extends FeatureSpec with GivenWhenThen with MustMatc
                  <Project name="PipelineA :: Stage5 :: accept" activity="Sleeping" lastBuildStatus="Failure" lastBuildLabel="113" lastBuildTime="2010-08-17T18:55:37" webUrl="http://10.23.30.42:8153/cruise/tab/build/detail/BonusSystem/113/SeleniumTest/1/SeleniumTest" />
                  <Project name="PipelineA :: Stage6" activity="Sleeping" lastBuildStatus="Success" lastBuildLabel="106" lastBuildTime="2010-08-16T14:08:39" webUrl="http://10.23.30.42:8153/cruise/tab/stage/detail/BonusSystem/106/PerformanceTest/1" />
                  <Project name="PipelineA :: Stage6 :: performance" activity="Sleeping" lastBuildStatus="Success" lastBuildLabel="106" lastBuildTime="2010-08-16T14:08:39" webUrl="http://10.23.30.42:8153/cruise/tab/build/detail/BonusSystem/106/PerformanceTest/1/PT" />
-                 <Project name="PipelineB :: Stage1" activity="Sleeping" lastBuildStatus="Success" lastBuildLabel="6" lastBuildTime="2010-08-17T19:03:35" webUrl="http://10.23.30.42:8153/cruise/tab/stage/detail/BonusTestTool/6/BuildUnitTest/1" />
-                 <Project name="PipelineB :: Stage1 :: buildAndUnitTest" activity="Sleeping" lastBuildStatus="Success" lastBuildLabel="6" lastBuildTime="2010-08-17T19:03:35" webUrl="http://10.23.30.42:8153/cruise/tab/build/detail/BonusTestTool/6/BuildUnitTest/1/build" />
+                 <Project name="PipelineB :: InitialStage" activity="Sleeping" lastBuildStatus="Success" lastBuildLabel="6" lastBuildTime="2010-08-17T19:03:35" webUrl="http://10.23.30.42:8153/cruise/tab/stage/detail/BonusTestTool/6/BuildUnitTest/1" />
+                 <Project name="PipelineB :: InitialStage :: buildAndUnitTest" activity="Sleeping" lastBuildStatus="Success" lastBuildLabel="6" lastBuildTime="2010-08-17T19:03:35" webUrl="http://10.23.30.42:8153/cruise/tab/build/detail/BonusTestTool/6/BuildUnitTest/1/build" />
                </Projects>
 
   feature("can create a pipeline from xml") {
@@ -49,6 +49,15 @@ class CruisePipelineBDDSpec extends FeatureSpec with GivenWhenThen with MustMatc
 
       and("the length will be correct")
       pipeline.stages.length must be === 1
+
+      and("the stage name will be correct")
+      pipeline.stages(0).name === "InitialStage"
+
+      and("the number of jobs will be correct")
+      pipeline.stages(0).jobs.length === 1
+
+      and("the name of the job will be correct")
+      pipeline.stages(0).jobs(0).name === "buildAndUnitTest"
     }
 
   }
